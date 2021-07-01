682d720354699662d27c80aa0b36a005dd05b537
QasimzadeElgun <67157999+QasimzadeElgun@users.noreply.github.com>
2021-07-01 18:45:59 +0400

    Done

--- javaapplication3/PercentageElements.java
+++ javaapplication3/PercentageElements.java
@@ -5,6 +5,8 @@
  */
 package javaapplication3;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 /**
@@ -13,52 +15,47 @@ import java.util.Scanner;
  */
 public class PercentageElements {
 
- 
+    public static void main(String[] args) {
 
-     public static void main(String[] args) {
- 
-         int upper = 0;
-         int lower = 0;
-         int spaces = 0;
-         int digit = 0;
-         int other = 0;
-         int size = 0;
- 
- 
-         Scanner scanner = new Scanner(System.in);
-         System.out.println("Please enter characters that you want to get percentage of them : ");
-         String name = scanner.nextLine();
- 
- 
-         char[] myCharacters = name.toCharArray();
- 
-        
- 
-         for (char c : myCharacters){
- 
-             if (Character.isUpperCase(c)){
-                 upper++;
-             }else if (Character.isLowerCase(c)){
-                 lower++;
-             }else  if (Character.isDigit(c)){
-                 digit++;
-             }else if (Character.isSpaceChar(c)){
-                 spaces++;
-             }else{
-                 other++;
-             }
- 
-         }
-         size = name.length();
-         System.out.println("Number of Uppercase letters :"  +upper+" Percentage of Uppercase letters :"+ ((upper * 100) / size) + "%");
-         System.out.println("Number of Lowercase letters :"  +lower+" Percentage of Lowercase letters :"+  ((lower * 100) / size) + "%");
-         System.out.println("Number of Digits letters :"  +digit+" Percentage of Digits letters :"+  ((digit * 100) / size) + "%");
-         System.out.println("Number of Special characters :"  +(spaces+other)+" Percentage of lowSpecialercase letters :"+  (((spaces+other) * 100) / size) + "%");
- 
- 
-     }
- 
- 
- 
- 
+        int upper = 0;
+        int lower = 0;
+        int digit = 0;
+        int other = 0;
+        int size = 0;
+
+        //Dynamic Code(if you want, you can run this code for dynamic results.
+              //Scanner scanner = new Scanner(System.in);
+              //System.out.println("Please enter characters that you want to get percentage of them : ");
+              //String name = scanner.nextLine();
+        //Static code
+        String name = "Tiger Runs @ The Speed Of 100 km/hour.";
+        char[] myCharacters = name.toCharArray();
+
+        for (char c : myCharacters) {
+
+            if (Character.isUpperCase(c)) {
+                upper++;
+            } else if (Character.isLowerCase(c)) {
+                lower++;
+            } else if (Character.isDigit(c)) {
+                digit++;
+            } else {
+                other++;
+            }
+
+        }
+        size = name.length();
+        NumberFormat formatter = new DecimalFormat("#0.00");
+        String upperPercantage = formatter.format(((double) (upper * 100) / size));
+        String lowerPercantage = formatter.format(((double) (lower * 100) / size));
+        String digitPercantage = formatter.format(((double) (digit * 100) / size));
+        String othersPercantage = formatter.format(((double) (other * 100) / size));
+
+        System.out.println("Number of uppercase letters is " + upper + ". " + "So percentage is " + upperPercantage + "%");
+        System.out.println("Number of lowercase letters is " + lower + ". " + "So percentage is " + lowerPercantage + "%");
+        System.out.println("Number of digits is " + digit + ". " + "So percentage is " + digitPercantage + "%");
+        System.out.println("Number of other characters is " + other + ". " + "So percentage is " + othersPercantage + "%");
+
+    }
+
 }
--- javaapplication3/SortArray.java
+++ javaapplication3/SortArray.java
@@ -6,61 +6,57 @@
 package javaapplication3;
 
 import java.util.List;
- 
- import java.util.ArrayList;
- import java.util.Arrays;
- import java.util.Collections;
- import java.util.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *
- * @author eqasm
+ * @author Elgun
  */
 public class SortArray {
-   
- 
- 
-     public static void main(String[] args) {
- 
- 
-         List<Integer> list = Arrays.asList(1,1,1,2,2,2,3,3,1,3,4,4,5,6,45,6,4,7,5,2,7,8,5,27,78,2,4);
- 
-         System.out.println(returnUniqueSortedArray2(list));
- 
-     }
- 
- 
-     public static List<Integer> returnUniqueSortedArray(List<Integer> list) {
-         List<Integer> returnElement = new ArrayList<>();
-         for (int i : list) {
-             if (!returnElement.contains(i)) {
-                 returnElement.add(i);
-             }
-         }
-         return returnElement;
-     }
- 
- 
-     public static List<Integer> returnUniqueSortedArray2(List<Integer> list) {
-         List<Integer> returnElement = new ArrayList<>();
- 
-         for (int i : list) {
-             int counter = 0;
-             for (int j = 0; j < returnElement.size(); j++) {
-                 if (returnElement.get(j) == i) {
-                     counter++;
-                 }
-             }
-             if (counter == 0) {
-                 returnElement.add(i);
-             }
- 
-         }
-         Collections.sort(returnElement);
-         return returnElement;
- 
-     }
- 
- 
- }
 
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(2,3,6,6,8,9,10,10,10,12,12);
+           //First Method
+        //System.out.println(SortArray(list));
+           //Second Method
+         System.out.println(SortArray2(list));
+    }
+
+    //First Method
+    public static List<Integer> SortArray(List<Integer> list) {
+        List<Integer> returnElement = new ArrayList<>();
+        for (int i : list) {
+            if (!returnElement.contains(i)) {
+                returnElement.add(i);
+            }
+        }
+        return returnElement;
+    }
+
+    //Second Method
+    public static List<Integer> SortArray2(List<Integer> list) {
+        List<Integer> returnElement = new ArrayList<>();
+
+        for (int i : list) {
+            int counter = 0;
+            for (int j = 0; j < returnElement.size(); j++) {
+                if (returnElement.get(j) == i) {
+                    counter++;
+                }
+            }
+            if (counter == 0) {
+                returnElement.add(i);
+            }
+
+        }
+        Collections.sort(returnElement);
+        return returnElement;
+
+    }
+
+}
